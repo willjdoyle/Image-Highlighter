70bb87870a3e7bed069a6321d75b7a59c91f9d96
willjdoyle <willjdoyle@gmail.com>
2020-06-01 11:43:58 -0400

    Updated to v1.2

--- README.md
+++ README.md
@@ -1,17 +1,63 @@
 # Image-Highlighter
 
-Image Highlighter has the user input a .jpg or .png file, specify a certain color (and tolerance from that color), and then the progarm will output the target image with the specified color highlighted with one of four preset options. This is currently version one. Entirely designed by myself using some MATLAB libraries.
+Image Highlighter has the user input a .jpg, .png, .tiff, or .gif file, specify a certain color (and tolerance from that color), and then the program will output the target image with the target color pixels highlighted. Image Highlighter is currently in v1.1.
 
 ## Installation
 
-Download the .m file and open it via MATLAB R2019B or later.
+Download the ImageHighlighter.m file and open it via MATLAB R2019B or later.
 
 ## How to Run
 
+Start ImageHighlighter.m from the MATLAB script header and follow the prompts.
+
 ### Inputs
 
-Start Image Highlighter from the MATLAB script header and follow the prompts. First it will ask for the file pathway to the .png/.jpg image. This will be in the format: C:\Users\user\Pictures\pic.jpg. Next, enter the hex color value that you would like to highlight without the "#". You will then be asked for the "absolute RGB tolerance". This is just the +/- offset from the target RGB value you entered that the program will still highlight. For example, if your hex value converted to (200, 170, 0) and your tolerance was 30, then it would highlight any pixel from (170, 140, 0) to (230, 200, 30). Finally, you need to enter either "t", "p", "b", or "w" for the highlighting color. This will replace pixels in the tolerance with that color (pink, black, or white). The "target" color is the hex color that you entered originally.
+First it will ask for the file pathway to the .png/.jpg image. Below is an example input: 
+```
+Please enter the image pathway: C:\Users\user\Pictures\pic.jpg
+```
+
+Next, enter the hex color value that you would like to highlight without the "#". The input is not case-sensitive. Below is an example:
+```
+Please enter the hex value of the color you want to highlight: #eb4034
+```
+
+You will then be asked for the "absolute RGB tolerance". This is just the +/- offset from the target RGB value you entered, and the program will highlight pixels in this range. For example, if your hex value converted to RGB was (200, 170, 0) and your tolerance was 30, then it would highlight any pixel from (170, 140, 0) to (230, 200, 30). A tolerance of 0 means that only pixels exactly matching the hex value will be highlighted. An input example is shown below:
+```
+Enter the absolute RGB tolerance (default: +/- 32): 20
+```
+
+Finally, you need to enter either "t", "p", "b", or "w" for the highlighting color for non-.gif's. .gif's are currently only able to be highlighted in black. The highlight color will replace pixels in the tolerance with that color (pink, black, or white). The "target" color is the hex color that you entered originally.
+Here is an example for a .jpg, .png, or .tiff input:
+```
+Please enter the highlighting color: target(t)/pink(p)/black(b)/white(w)/other hex #: p
+```
+For .gif's, the following output will display to console with no input necessary.
+```
+.gif highlighting can only be done in black.
+```
 
 ### Outputs
 
-This will then output a MATLAB figure with the appropriately highlighted image. It does not modify the source image.
+For .jpg's, .png's, and .tiff's, the output will be a MATLAB figure with the appropriately highlighted image.
+For .gif's, the output will be a slideshow of each frame in the MATLAB Movie Player.
+
+### Issues
+
+## Issue Templates
+To submit a bug report or request a feature, please use the templates located in [template folder](.github/ISSUE_TEMPLATE).
+
+## Known Issues
+There is currently a known problem that can occur with .gif's where certain frames may be dropped because there is an error reading them. I am currently investigating this and it will be addressed in the v1.2.
+
+### Versioning
+
+Image Highlighter is currently on v1.1. A full changelog can be found in the Dev Roadmap in the Projects tab.
+
+### Authors
+
+Image Highlighter was developed in its entirity by William Doyle.
+
+### Licsense
+
+This project is licensed under the GNU AGPL v3.0 License. Please see [LICENSE.md](LICENSE.md) for more details.
--- imageTest.m
+++ imageTest.m
@@ -1,62 +1,121 @@
-%Image Highlighting Program V1.0
+%Image Highlighting Program V1.1
 %William Doyle
 %This program highlights similar-colored pixels to the ones entered by the
 %user. Future configuration options to be used. Confirm to work with .jpg
 %and .png, should work for other formats but have not yet tested it.
+%This program is licensed under GNU AGPL v3.0
 
 %image input
-imgPath = input('Please enter the .jpg or .png image pathway: ', 's');
-[img, cmap] = imread(imgPath);
+imgPath = input('Please enter the image pathway: ', 's');
+
+%handshake to determine image type because MATLAB does not allow indexing
+%into temporary array
+temp = imfinfo(imgPath); 
+fileType = temp(1).Format();
+
+imgGifArray = []; %empty array for remaking .gif's
+
+%converting to index image and getting width, height, and frames (for .gif's)
+if(fileType == 'GIF')
+    [img, cmap] = imread(imgPath, 'frames', 'all');
+    temp = size(img);
+    numFrames = temp(4); %.gif frames
+else
+    [img, cmap] = imread(imgPath);
+    numFrames = 1; %default value (1 frame for non-.gif's)
+end
 width = size(img, 2);
 height = size(img, 1);
 
 hexInput = input('Please enter the hex value of the color you want to highlight: #', 's'); %user-input hex
-tol = input('Enter the absolute RGB tolerance (default: +/- 32): '); %variation in r, g, b values from hexInput
 
-defaultColor = input('Please enter the highlighting color: target(t)/pink(p)/black(b)/white(w): ', 's');
-
-%converting hex to rgb
+%converting user-input hex to rgb
 userR = hex2dec(hexInput(1:2));
 userG = hex2dec(hexInput(3:4));
 userB = hex2dec(hexInput(5:6));
 
-%highlighting color switch
-switch(defaultColor)
-    case 't'
-        defaultR = userR;
-        defaultG = userG; %add counter to count how many pixels were changed, as # and percent
-        defaultB = userB;
-    case 'p'
-        defaultR = 255;
-        defaultG = 0;
-        defaultB = 127;
-    case 'b'
-        defaultR = 0;
-        defaultG = 0;
-        defaultB = 0;
-    case 'w'
-        defaultR = 255;
-        defaultG = 255;
-        defaultB = 255;
-    otherwise
-        fprintf('Default color not correctly chosen. Defaulting to target.');
-        defaultR = userR;
-        defaultG = userG;
-        defaultB = userB;
-end
+tol = input('Enter the absolute RGB tolerance (default: +/- 32): '); %variation in r, g, b values from hexInput
 
-%main loop to amplify colors and increment color counts
-for i = 1:1:height
-    for j = 1:1:width
-        if((img(i, j, 1) >= userR - tol && img(i, j, 1) <= userR + tol) && ... %fits red tolerance
-           (img(i, j, 2) >= userG - tol && img(i, j, 2) <= userG + tol) && ... %fits green tolerance
-           (img(i, j, 3) >= userB - tol && img(i, j, 3) <= userB + tol)) %fits blue tolerance
-        
-            img(i, j, 1) = defaultR;
-            img(i, j, 2) = defaultG;
-            img(i, j, 3) = defaultB;
+%highlight color input is split because .gif's can only handle 
+%black easily without decreasing efficieny from O(mno) to O(mnop)
+if(fileType == 'GIF')
+    fprintf('.gif highlighting can only be done in black.\n');
+    defaultGifAddress = length(cmap); %pointing to black in cmap (always first index)
+else
+    defaultColor = input('Please enter the highlighting color: target(t)/pink(p)/black(b)/white(w)/other hex #: ', 's');
+    
+        switch(defaultColor)
+        case 't'
+            defaultR = userR;
+            defaultG = userG;
+            defaultB = userB;
+        case 'p'
+            defaultR = 255;
+            defaultG = 0;
+            defaultB = 127;
+        case 'b'
+            defaultR = 0;
+            defaultG = 0;
+            defaultB = 0;
+        case 'w'
+            defaultR = 255;
+            defaultG = 255;
+            defaultB = 255;
+        otherwise
+            defaultR = hex2dec(defaultColor(1:2));
+            defaultG = hex2dec(defaultColor(3:4));
+            defaultB = hex2dec(defaultColor(5:6));
         end
-     end
 end
 
-imshow(img, cmap)
+%main loop to amplify colors and increment pixel change count
+for currentFrame  = 1:1:numFrames
+    count = 0; %count of changed pixels
+    
+    if(fileType == 'GIF')
+        currentImg = img(:,:,1,currentFrame); %current frame of .gif, if there is a .gif
+    end
+    
+    for i = 1:1:height
+        for j = 1:1:width
+            %process .gif's here frame-by-frame
+            if(fileType == 'GIF')
+                if(255*cmap(1+currentImg(i,j),1) >= userR - tol && 255*cmap(1+currentImg(i,j),1) <= userR + tol) && ... %fits red tolerance
+               (255*cmap(1+currentImg(i,j),2) >= userG - tol && 255*cmap(1+currentImg(i,j),2) <= userG + tol) && ... %fits green tolerance
+               (255*cmap(1+currentImg(i,j),3) >= userB - tol && 255*cmap(1+currentImg(i,j),3) <= userB + tol) %fits blue tolerance
+
+                currentImg(i, j) = defaultGifAddress;
+
+                count = count + 1; %incrementing # changed pixels
+                end
+            %all other non-.gif's are processed here
+            else
+                if((img(i, j, 1) >= userR - tol && img(i, j, 1) <= userR + tol) && ... %fits red tolerance
+                   (img(i, j, 2) >= userG - tol && img(i, j, 2) <= userG + tol) && ... %fits green tolerance
+                   (img(i, j, 3) >= userB - tol && img(i, j, 3) <= userB + tol)) %fits blue tolerance
+
+                    img(i, j, 1) = defaultR;
+                    img(i, j, 2) = defaultG;
+                    img(i, j, 3) = defaultB;
+
+                    count = count + 1; %incrementing # changed pixels
+                end
+            end
+        end
+    end
+    
+    if(fileType == 'GIF')
+        imgGifArray = cat(4, imgGifArray, currentImg); %array of .gif frames appended to each other on dimension 4
+    end
+
+    fprintf("For frame %d, %d pixels were highlighted, or %.2f%% of the total.\n", currentFrame, count, count/(width*height)*100);
+    
+    
+end
+
+%printing modified image/gif
+if(fileType == 'GIF')
+    implay(immovie(imgGifArray, cmap)) %uses matlab movie player
+else
+    imshow(img, cmap)
+end
